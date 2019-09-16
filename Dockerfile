db37605a53577dec018b1620376cef8049193433
Drew Brantley <brantley@us.ibm.com>
2019-09-16 13:52:26 -0500

    add https

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM node:10.16.0
 
+
 # put this just after node so we can reuse the pm2 image 
-RUN npm i -g pm2
+# RUN npm i -g pm2
 
 WORKDIR /usr/src/app
+
+# RUN mkdir config && mkdir config/key && cd config/key && openssl req -nodes -new -x509 -keyout server.key -out server.cert
 
 COPY package*.json ./
 
@@ -11,4 +14,5 @@ RUN npm i
 
 COPY . .
 
-CMD ["pm2-runtime","server.js","--name","exposed_http_server"]
+# CMD ["pm2-runtime","server.js","--name","exposed_http_server"]
+CMD npm start  
--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
+var bodyParser = require('body-parser')
+var fs = require('fs')
+var https = require('https')
+
+const PORT = 3000;
+
 const {router:file_router} = require('./file_router.js');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use((req, res, next)=>{
+	if( req.secure===false ){
+		return res.json({"err":"https only"});
+	}else{
+		return next();
+	}
+});
+
 app.use(file_router);
 
-app.set("port", process.argv[2] || 3000);
+app.set("port", process.argv[2] || PORT);
 
-app.listen(app.get('port'),()=>{
-	console.log("listening on port "+app.get('port'));
+https.createServer({
+	key: fs.readFileSync('./config/key/server.key'),
+	cert: fs.readFileSync('./config/key/server.cert')
+  }, app)
+.listen(app.get('port'), function(){
+	console.log(`Example app listening on port ${app.get('port')}! Go to https://localhost:${app.get('port')}/`)
 });
