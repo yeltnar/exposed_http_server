8421c9b2e2acc24a154395a2ce10d1bc2c3396ab
Drew Brantley <brantley@us.ibm.com>
2019-09-24 21:43:15 -0500

    adding fallback

--- file_router.js
+++ file_router.js
@@ -1,7 +1,27 @@
 const express = require("express");
+const fs = require("fs");
 
 const router = express.Router();
 
-router.use("/",express.static("./mnt"));
+router.use("/",express.static("./mnt",{fallthrough:true}));
+
+router.use((req,res,next)=>{
+    
+    const {originalUrl} = req;
+
+    let dir_contents;
+    let err;
+    try{
+        dir_contents = fs.readdirSync(`./mnt${originalUrl}`);
+    }catch(e){
+        err = e;
+    }
+
+    res.json({
+        originalUrl,
+        dir: dir_contents,
+        err
+    })
+})
 
 module.exports = {router}
